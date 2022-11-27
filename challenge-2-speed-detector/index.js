f4c94fa09b93c672e20555308823b9c54e0da8e6
mknyambura <kariuki.mercie@gmail.com>
2022-11-27 10:32:59 +0300

    Update commit

--- challenge-2-speed-detector/index.js
+++ challenge-2-speed-detector/index.js
@@ -1,4 +1,4 @@
-function checkspeed() {
+function checkSpeed() {
     const speed = document.getElementById('s').value;
     document.getElementById('s').innerHTML = `Your speed is ${speed}`
     
@@ -8,9 +8,10 @@ function checkspeed() {
     if (speed <= 70){
         // window.alert('Ok')
         document.getElementById('calc').innerHTML = `Ok`
-    }else if (speed > 70 && points >= 12){
+        document.getElementById('credit').innerHTML = `You're good to go!`
+    }else if (points >= 0.05 && points < 12){
+        document.getElementById('credit').innerHTML = `Warning!`
+    }else if (points >= 12){
         document.getElementById('credit').innerHTML = `License suspended`;
     }
-       
-    
 }
